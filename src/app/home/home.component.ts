9bb29bbe14d357d45d4933e7c6b3c771b7007e72
osama mahmoud rashed <osamamahmoudrashed@gmail.com>
2024-04-23 13:18:22 +0200

    task service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ShareserviceService } from '../service/shareservice.service';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +9,8 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private route:Router) {    
+  constructor(private route:Router,private share:ShareserviceService) {    
+    share.print()
   }
   ngOnInit(): void {
   }
