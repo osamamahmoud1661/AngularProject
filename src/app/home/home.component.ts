a9b5f955bd154d5533d0de9c1eedb01423e8b7bb
osama mahmoud rashed <osamamahmoudrashed@gmail.com>
2024-04-23 17:00:31 +0200

    make validation on form

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ShareserviceService } from '../service/shareservice.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -54,5 +55,8 @@ gotodetails(){
 
   }
 
-
+save(f:NgForm){
+  let data = f.value
+  console.log(data)
+}
 }
